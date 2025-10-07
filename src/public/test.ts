c6a625313062d6c3a44468ced5cf0bc3a16aab09
lzw. <lzwdot@qq.com>
2025-10-07 11:59:23 +0800

    更新装饰器模式文档，补充特点说明、基本使用示例、装饰class/method实现和AOP应用场景

--- src/public/test.ts
+++ src/public/test.ts
@@ -1,9 +1,36 @@
-const obj = {};
-console.log(obj.__proto__ === Object.prototype); // true
+// 普通写法
+function log1() {
+  console.log('记录日志');
+}
 
-const arr = [];
-console.log(arr.__proto__ === Array.prototype); // true
+class Foo1 {
+  fn() {
+    log1();
+    console.log('业务功能-点赞');
+  }
+}
+const foo1 = new Foo1();
+console.log(foo1.fn());
 
-function Person() {}
-const person = new Person();
-console.log(person.__proto__ === Person.prototype); // true
+// 装饰器写法
+function log2(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  const oldMethod = descriptor.value; // 保存原有 fn 方法
+  
+  // 重写 Foo2.fn 方法
+  descriptor.value = function() {
+    console.log('记录日志');
+    return oldMethod.apply(this, arguments);
+  };
+  
+  return descriptor;
+}
+
+class Foo2 {
+  @log2
+  fn() {
+    console.log('业务功能-点赞');
+  }
+}
+
+const foo2 = new Foo2();
+console.log(foo2.fn());
